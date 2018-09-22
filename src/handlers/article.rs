7d6e129cab5c9dd1b32288474fbd77dcb09cccac
mtwtkman <punipuniomochi@gmail.com>
2018-09-22 13:05:47 +0900

    added specified article handler

--- src/handlers/article.rs
+++ src/handlers/article.rs
@@ -5,35 +5,36 @@ use diesel::QueryDsl;
 use models::{schema, Article, Tag, Tagging};
 use rocket::http::Status;
 use rocket::response::status::NotFound;
+use rocket::response::Responder;
 use rocket_contrib::Json;
 use DbConn;
 
-#[get("/articles")]
+#[get("/")]
 pub fn all(conn: DbConn) -> Json<Vec<Article>> {
     let article_list = schema::articles::table
         .order(schema::articles::created_at.desc())
         .load(&*conn)
-        .unwrap();
+        .unwrap_or(vec![]);
     Json(article_list)
 }
 
-#[get("/articles/<id>")]
-pub fn one(id: i32, conn: DbConn) -> Json<(Article, Vec<Tag>)> {
-    let article: Article = schema::articles::table
+#[get("/<id>")]
+pub fn one(id: i32, conn: DbConn) -> Result<Json<(Article, Vec<Tag>)>, NotFound<String>> {
+    schema::articles::table
         .find(id)
         .first::<Article>(&*conn)
-        .map_err(|_| NotFound("oh".to_string()))
-        .unwrap();
-    let tag_ids: Vec<i32> = Tagging::belonging_to(&article)
-        .select(schema::taggings::tag_id)
-        .load::<i32>(&*conn)
-        .map_err(|_| NotFound("oh".to_string()))
-        .unwrap();
-    schema::tags::table
-        .filter(schema::tags::id.eq_any(&tag_ids))
-        .order(schema::tags::id)
-        .load::<Tag>(&*conn)
-        .map_err(|_| NotFound("oh".to_string()))
-        .map(|tags| Json((article, tags)))
-        .unwrap()
+        .map_err(|_| NotFound(format!("article_id: {}", id)))
+        .and_then(|article| {
+            Tagging::belonging_to(&article)
+                .select(schema::taggings::tag_id)
+                .load::<i32>(&*conn)
+                .or(Ok(vec![]))
+                .and_then(|tag_ids| {
+                    schema::tags::table
+                        .filter(schema::tags::id.eq_any(&tag_ids))
+                        .order(schema::tags::id)
+                        .load::<Tag>(&*conn)
+                }).or(Ok(vec![]))
+                .map(|tags| Json((article, tags)))
+        })
 }
