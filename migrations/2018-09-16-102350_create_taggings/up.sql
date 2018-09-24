bdbadcbae68eda0b34f05e779cdc5fb45dd1f8df
mtwtkman <punipuniomochi@gmail.com>
2018-09-24 19:55:26 +0900

    added constraint

--- migrations/2018-09-16-102350_create_taggings/up.sql
+++ migrations/2018-09-16-102350_create_taggings/up.sql
@@ -1,7 +1,8 @@
 create table taggings (
   id integer primary key autoincrement not null,
-  tag_id integer not null,
-  article_id integer not null,
-  constraint uq_taggings unique (tag_id, article_id)
+  tag_id integer not null references tags(id) on delete cascade,
+  article_id integer not null references articles(id) on delete cascade,
+  constraint uq_taggings
+    unique (tag_id, article_id)
 )
 ;
